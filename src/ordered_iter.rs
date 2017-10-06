ffa04d667d19d44b32b08af8e4b1d48841a57c7f
subdir <subdir@gmail.com>
2017-10-07 01:53:28 +0300

    refactored ordered_iter

--- src/ordered_iter.rs
+++ src/ordered_iter.rs
@@ -1,82 +1,108 @@
 use std::iter::Peekable;
 
 
-pub trait StrictAscIter where Self: Iterator, Self::Item: Ord {}
-pub trait StrictDescIter where Self: Iterator, Self::Item: Ord {}
+pub trait OrderedIterator
+where
+    Self: Iterator,
+    Self::Item: Ord,
+    Self::Order: Order
+{
+    type Order;
+}
 
 
-pub trait SortedIterator {
-    fn ensure_strict_asc(self) -> EnsureStrictAsc<Self>
+pub trait OrderEnsurableIterator {
+    fn ensure_order<O>(self, order: O) -> EnsureOrder<O, Self>
     where
         Self: Iterator + Sized,
-        Self::Item: Ord
+        Self::Item: Ord,
+        O: Order
     {
-        EnsureStrictAsc::new(self)
-    }
-
-    fn ensure_strict_desc(self) -> EnsureStrictDesc<Self>
-    where
-        Self: Iterator + Sized,
-        Self::Item: Ord
-    {
-        EnsureStrictDesc::new(self)
+        EnsureOrder::new(order, self)
     }
 }
 
 
-impl<T> SortedIterator for T where T: Iterator {}
+impl<T> OrderEnsurableIterator for T where T: Iterator {}
 
 
-pub struct EnsureStrictAsc<I> where I: Iterator, I::Item: Ord {
-    iter: Peekable<I>
-}
-
-impl<I> EnsureStrictAsc<I> where I: Iterator, I::Item: Ord {
-    fn new(iter: I) -> Self { Self { iter: iter.peekable() } }
-}
-
-impl<I> StrictAscIter for EnsureStrictAsc<I> where I: Iterator, I::Item: Ord {}
-
-impl<I> Iterator for EnsureStrictAsc<I> where I: Iterator, I::Item: Ord {
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        ensured_order_next(&mut self.iter, |a, b| a < b)
-    }
+pub trait Order {
+    fn holds<T: Ord>(&self, a: &T, b: &T) -> bool;
 }
 
 
-pub struct EnsureStrictDesc<I> where I: Iterator, I::Item: Ord {
-    iter: Peekable<I>
-}
-
-impl<I> EnsureStrictDesc<I> where I: Iterator, I::Item: Ord {
-    fn new(iter: I) -> Self { Self { iter: iter.peekable() } }
-}
-
-impl<I> StrictDescIter for EnsureStrictDesc<I> where I: Iterator, I::Item: Ord {}
-
-impl<I> Iterator for EnsureStrictDesc<I> where I: Iterator, I::Item: Ord {
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        ensured_order_next(&mut self.iter, |a, b| a > b)
-    }
+pub struct Ascending;
+impl Order for Ascending {
+    fn holds<T: Ord>(&self, a: &T, b: &T) -> bool { a <= b }
 }
 
 
-fn ensured_order_next<I, F>(iter: &mut Peekable<I>, assertion: F) -> Option<I::Item>
+pub struct Descending;
+impl Order for Descending {
+    fn holds<T: Ord>(&self, a: &T, b: &T) -> bool { a >= b }
+}
+
+
+pub struct StrictAscending;
+impl Order for StrictAscending {
+    fn holds<T: Ord>(&self, a: &T, b: &T) -> bool { a < b }
+}
+
+
+pub struct StrictDescending;
+impl Order for StrictDescending {
+    fn holds<T: Ord>(&self, a: &T, b: &T) -> bool { a > b }
+}
+
+
+pub struct EnsureOrder<O, I>
 where
     I: Iterator,
-    F: Fn(&I::Item, &I::Item) -> bool,
+    I::Item: Ord,
+    O: Order
 {
-    match iter.next() {
-        None => None,
-        Some(next) => match iter.peek() {
-            None => Some(next),
-            Some(peek) => {
-                assert!(assertion(&next, &peek));
-                Some(next)
+    iter: Peekable<I>,
+    order: O
+}
+
+
+impl<O, I> EnsureOrder<O, I>
+where
+    I: Iterator,
+    I::Item: Ord,
+    O: Order
+{
+    fn new(order: O, iter: I) -> Self { Self { iter: iter.peekable(), order: order } }
+}
+
+
+impl<O, I> OrderedIterator for EnsureOrder<O, I>
+where
+    I: Iterator,
+    I::Item: Ord,
+    O: Order
+{
+    type Order = O;
+}
+
+
+impl<O, I> Iterator for EnsureOrder<O, I>
+where
+    I: Iterator,
+    I::Item: Ord,
+    O: Order
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            None => None,
+            Some(next) => match self.iter.peek() {
+                None => Some(next),
+                Some(peek) => {
+                    assert!(self.order.holds(&next, &peek));
+                    Some(next)
+                }
             }
         }
     }
@@ -90,29 +116,57 @@ mod tests {
     #[test]
     fn test_asc_ok() {
         assert_eq!(
-            vec!(1u8,2,3).into_iter().ensure_strict_asc().collect::<Vec<u8>>(),
+            vec!(1u8,2,2).into_iter().ensure_order(Ascending).collect::<Vec<u8>>(),
+            vec!(1u8,2,2)
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed: self.order.holds(&next, &peek)")]
+    fn test_asc_fail() {
+        vec!(1u8,2,1).into_iter().ensure_order(Ascending).collect::<Vec<u8>>();
+    }
+
+    #[test]
+    fn test_strict_asc_ok() {
+        assert_eq!(
+            vec!(1u8,2,3).into_iter().ensure_order(StrictAscending).collect::<Vec<u8>>(),
             vec!(1u8,2,3)
         );
     }
 
     #[test]
-    #[should_panic(expected = "assertion failed: assertion(&next, &peek)")]
-    fn test_asc_fail() {
-        vec!(1u8,2,2).into_iter().ensure_strict_asc().collect::<Vec<u8>>();
+    #[should_panic(expected = "assertion failed: self.order.holds(&next, &peek)")]
+    fn test_strict_asc_fail() {
+        vec!(1u8,2,2).into_iter().ensure_order(StrictAscending).collect::<Vec<u8>>();
     }
 
     #[test]
     fn test_desc_ok() {
         assert_eq!(
-            vec!(3u8,2,1).into_iter().ensure_strict_desc().collect::<Vec<u8>>(),
+            vec!(3u8,3,1).into_iter().ensure_order(Descending).collect::<Vec<u8>>(),
+            vec!(3u8,3,1)
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed: self.order.holds(&next, &peek)")]
+    fn test_desc_fail() {
+        vec!(3u8,2,3).into_iter().ensure_order(Descending).collect::<Vec<u8>>();
+    }
+
+    #[test]
+    fn test_strict_desc_ok() {
+        assert_eq!(
+            vec!(3u8,2,1).into_iter().ensure_order(StrictDescending).collect::<Vec<u8>>(),
             vec!(3u8,2,1)
         );
     }
 
     #[test]
-    #[should_panic(expected = "assertion failed: assertion(&next, &peek)")]
-    fn test_desc_fail() {
-        vec!(3u8,2,2).into_iter().ensure_strict_desc().collect::<Vec<u8>>();
+    #[should_panic(expected = "assertion failed: self.order.holds(&next, &peek)")]
+    fn test_strict_desc_fail() {
+        vec!(3u8,2,2).into_iter().ensure_order(StrictDescending).collect::<Vec<u8>>();
     }
 }
 
